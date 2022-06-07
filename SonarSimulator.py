8e041510579ab8990580f3fccb86a9d68a97bf8c
Jacobb13 <jacobbellavance@cidco.ca>
2022-06-07 19:01:47 +0000

    Correction

--- SonarSimulator.py
+++ SonarSimulator.py
@@ -25,45 +25,23 @@ import usb.util
  ##IdProduct: 0x6001
  
 ## Rpi
- ##IdVendor: 0x067b
- ##idProduct: 0x2303
+VID=0x067b
+PID=0x2303
 ## "lsusb -v" sur le rpi pour connaitre les infos
 
 #bEndpointAddress : 0x02 (EP 2 OUT)
 
 
-dev = usb.core.find(idVendor=0x0403, idProduct=0x6001)
+dev = usb.core.find(idVendor=VID, idProduct=PID)
 if dev is None:
  raise ValueError('Our device is not connected')
-help(usb.util)
 
+print("ISsa connected")
 # set the active configuration. With no arguments, the first
 # configuration will be the active one
 dev.set_configuration()
 
-# get an endpoint instance
-cfg = dev.get_active_configuration()
-intf = cfg[(0,0)]
+dev.write("TEST")
+print("TERMINATE")
 
-ep = usb.util.find_descriptor(
- intf,
- # match the first OUT endpoint
- custom_match = \
- lambda e: \
- usb.util.endpoint_direction(e.bEndpointAddress) == \
- usb.util.ENDPOINT_OUT)
 
-assert ep is not None
-
-# write the data
-ep.write('test')    
-    
-    
-    
-    
-       
-#f = open("output.txt","w+")
-#for i in 10 :
-# f.append(i)
-# i+=1
- 
