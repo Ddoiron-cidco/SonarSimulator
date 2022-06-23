6de126dcc7b63455ffffab8229d38914cf496616
Jacobb13 <jacobbellavance@cidco.ca>
2022-06-23 19:49:06 +0000

    CLEAN UP

--- src/nmeaSimulator.py
+++ src/nmeaSimulator.py
@@ -15,28 +15,43 @@ import threading
 import time
 from random import random
 from sys import stdout
-
-
 DURATION = 1
 
-###### Main #########################
 def checkSum(data):
     sum=0
     for ch in data:
         sum^=ord(ch)
-    return f'{data}*{sum:02X}'
+    return f'{data}*{sum:02X}\r\n'
 
+def help():
+    # Display Help
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("Help")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("")
+    print("HOW TO RUN THE PROGRAM:")
+    print("Syntax: python3 p")
+    print("Options:")
+    print("")
+    print("-help or -h                           Print Help Page")
+    print(" 'GGA', 'GLL', 'GSA', 'GSV', 'RMC', 'VTG', 'ZDA'")
+    print("")
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print("IF NOT WORKING : ")
+    print("Check the baudrate of both devices : ")
+    print("for NEMA-0183, it should be configured at 4800")
+    print("To verify baudrate : stty -F /dev/ttyUSB0")
+    print("To set baudrate : stty -F /dev/ttyUSB0 4800")
+    print("BE SURE TO BE LAUNCHING THE PROGRAM ON THE Raspberry pi !")
 
+##################### Main ######################
 if len(sys.argv) > 1:
     # Open simulator and output file
     simulator = Simulator()
     serialPort = open("/dev/ttyUSB0", "w")
 
-#    with sim.lock:
-
     #Generate GGA
     # Can re-order or drop some
-#    sim.gps.output = tuple()
     simulator.gps.output = ('GGA', 'GLL', 'GSA', 'GSV', 'RMC', 'VTG', 'ZDA')
     simulator.gps.num_sats = 14
     simulator.gps.lat = 1
@@ -61,21 +76,14 @@ if len(sys.argv) > 1:
     simulator.heading_variation = 0
     #simulator.generate(3, serialPort)
     while True :
+        #Generate GPS
         simulator.generate(DURATION, serialPort)
-        #for sentence in simulator.get_output(DURATION):
-            #simulator.generate(DURATION,serialPort)
-            #print(type(sentence))
-            #serialPort.write(sentence)
-            #time.sleep(1)
-          # serialPort.write('\r\n')
-         
-    # Generate DBT
+        #Generate DBT
         depthMeters = 42.0 #TODO: generate from model
         depthFeet = float(depthMeters*3.28084)
         depthFathoms = float(depthMeters*0.546807)
         DBTstring = f'$SDDBT,{round(depthFeet,1)},f,{round(depthMeters,1)},M,{round(depthFathoms,1)},' 
         dbt=checkSum(DBTstring) #.encode('utf-8') 
-        #print(dbt)
         serialPort.write(dbt)
     serialPort.close()
 
@@ -83,6 +91,4 @@ else:
     help()  # not the right amount of argument
 
 
-def help():
-    print("Veuillez resayez")
 
