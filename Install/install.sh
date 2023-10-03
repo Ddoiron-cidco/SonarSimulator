5d0ed4ccc596e679ff71dcfbaa9023ea263762d0
Dany Doiron <dany.doiron@cidco.ca>
2023-10-03 18:17:27 +0000

    add dpt simulator only and unittest

---
+++ Install/install.sh
@@ -0,0 +1,16 @@
+sudo apt install python3 python-is-python3 python3-pip -y
+
+sudo bash -c 'cat << EOF3 > /etc/systemd/system/nmea.service
+[Unit]
+Description=Launch NMEA Simulator on boot.
+
+[Service]
+Type=simple
+ExecStart=/home/ubuntu/SonarSimulator/Script/autolaunch.sh
+EOF3'
+
+sudo chmod 755 /etc/systemd/system/nmea.service
+sudo systemctl enable nmea
+sudo systemctl start nmea
+sudo systemctl status nmea
+
---
+++ Script/autolaunch.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python /home/ubuntu/SonarSimulator/Script/sonar_simulator_DPT.py /dev/ttyUSB0 9600
